a0c08dd1373d051e0ba07b32dfc5282faca4e05b
raghu24082001 <raghudae248@gmail.com>
2026-04-04 18:31:55 +0530

    Day30

--- Assignment/Day30/memoize/src/App.jsx
+++ Assignment/Day30/memoize/src/App.jsx
@@ -1,41 +1,81 @@
 import React, { useMemo, useState } from 'react'
 
 const App = () => {
+  const [toggle, setToggle] = useState(false)
+  const [num, setNum] = useState(0)
+  const [name, setName] = useState('')
 
-  const [toggle, SetToogle] = useState(false)
-  const [num,setNum] = useState(0)
-  
-  
-
-  const handlechange = ( e) => {
-
+  // Generate numbers and sort in descending order
+  const generateNumbers = () => {
     let arr = []
-    for(let i=0; i<=num; i++){
+    for (let i = 0; i <= num; i++) {
       arr.push(i)
     }
-      
-     let newarr = arr.sort((a, b) => b - a)
-
-      console.log(newarr)
-
+    return arr.sort((a, b) => b - a)
   }
 
-const handleChange2 = (e) => {
+  // List of names
+  const listOfNames = ['sachin', 'saurav', 'virat', 'rohit', 'dhoni']
 
-}
+  // Filter names based on input
+  const filterNames = () => {
+    return listOfNames.filter((e) =>
+      e.includes(name)
+    )
+  }
 
+  // Memoized values
+  const memoizedNumbers = useMemo(() => generateNumbers(), [num])
+  const memoizedNames = useMemo(() => filterNames(), [name])
 
-  const memoizedValue = useMemo(() => handlechange(num), [num])
- 
-  
   return (
-    <>
-      <div className={toggle ? 'bg-red-300' : 'bg-yellow-300'}>Hello</div>
-      <button className='border rounded m-2 p-1' onClick={() => SetToogle(!toggle)}>Toggle Theme</button>
-      <input type="text" onChange={(e)=> setNum(e.target.value)}/>
-      <button className='border rounded m-2 p-1' onClick={()=> handlechange(num)}>Click me</button>
+    <div className="p-4">
+      {/* Theme toggle */}
+      <div className={toggle ? 'bg-red-300 p-2' : 'bg-yellow-300 p-2'}>
+        Hello
+      </div>
 
-    </>
+      <button
+        className="border rounded m-2 p-1"
+        onClick={() => setToggle(!toggle)}
+      >
+        Toggle Theme
+      </button>
+
+      {/* Number input */}
+      <div className="mt-4">
+        <input
+          type="number"
+          placeholder="Enter a number"
+          onChange={(e) => setNum(Number(e.target.value))}
+          className="border rounded p-1"
+        />
+
+        <ul className="mt-2">
+          {memoizedNumbers.map((e, i) => (
+            <li key={i}>{e}</li>
+          ))}
+        </ul>
+      </div>
+
+      {/* Name filter */}
+      <div className="mt-4">
+        <input
+          type="text"
+          placeholder="Search name"
+          onChange={(e) => setName(e.target.value)}
+          className="border rounded p-1"
+        />
+
+        <ul className="mt-2">
+          {memoizedNames.length > 0 ? (
+            memoizedNames.map((e, i) => <li key={i}>{e}</li>)
+          ) : (
+            <li>No match found</li>
+          )}
+        </ul>
+      </div>
+    </div>
   )
 }
 
