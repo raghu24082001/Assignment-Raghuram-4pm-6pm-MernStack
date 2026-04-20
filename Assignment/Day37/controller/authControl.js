4e79e4ea2e4078383f9543586be59cfd5f5ae927
raghu24082001 <raghudae248@gmail.com>
2026-04-20 13:07:18 +0530

    day37

--- Assignment/Day37/controller/authControl.js
+++ Assignment/Day37/controller/authControl.js
@@ -2,3 +2,8 @@ export const controlbody = (req,res)=>{
    console.log(req.body)
    res.status(200).json(req.body)
 }
+
+export const cgetparams = (req,res)=>{
+   console.log(req.params)
+   res.status(200).json(req.body)
+}
--- Assignment/Day37/router/authRouter.js
+++ Assignment/Day37/router/authRouter.js
@@ -1,10 +1,14 @@
 import express from 'express'
-import { controlbody } from '../controller/authcontrol.js'
+import { cgetparams, controlbody } from '../controller/authcontrol.js'
 
 const route = express.Router()
 
 //http://localhost:5000/api/user/bodyuser
 
 route.post('/bodyuser',controlbody)
+
+route.get('/bodyparams/:userid',cgetparams)
+
+//http://localhost:5000/api/user/bodyparams
 
 export default route
